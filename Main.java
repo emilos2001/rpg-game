06bb3db4a19be45b3eabec3220ee8d2f26b64e62
emilos2001 <140043844+emilos2001@users.noreply.github.com>
2023-09-13 15:18:56 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package Totorial.RPG;
+
+import javax.swing.*;
+
+public class Main {
+    //хуйня мой проиект знаю.
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        GamePanel gp = new GamePanel();
+        frame.setResizable(false);
+        frame.add(gp);
+        frame.pack();
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        gp.setupGame();
+        gp.startGame();
+
+    }
+}
